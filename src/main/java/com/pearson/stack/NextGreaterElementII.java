0870deb3a2e72c5256092f573ce1be2c92839f09
iamnps <niepeishen@sina.com>
2021-07-22 21:11:43 +0800

    NextGreaterElementII

--- src/main/java/com/pearson/stack/NextGreaterElementII.java
+++ src/main/java/com/pearson/stack/NextGreaterElementII.java
@@ -3,10 +3,10 @@ package com.pearson.stack;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 
 /**
- * leetcode 503
- * Created by peishen.nie on 2021/7/22.
+ * leetcode 503 Created by peishen.nie on 2021/7/22.
  */
 public class NextGreaterElementII {
 
@@ -15,28 +15,66 @@ public class NextGreaterElementII {
         elementII.nextGreaterElementII();
     }
 
-    private void nextGreaterElementII(){
-        System.out.println(Arrays.toString(nextGreaterElements(new int[]{1,23,5,6,8})));
+    private void nextGreaterElementII() {
+        System.out.println(Arrays.toString(nextGreaterElements(new int[] { 5, 4, 3, 2, 1 })));
     }
 
+    // hashmap还是慢
     public int[] nextGreaterElements(int[] nums) {
         int[] res = new int[nums.length];
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++){
-            deque.add(i);
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(i, nums[i]);
         }
-        for (int i = 0; i < nums.length; i++){
-            while (!deque.isEmpty()){
-                if(i != deque.peek()){
-                    int x = deque.poll();
-                    deque.addLast(x);
+        for (int i = 0; i < nums.length; i++) {
+            int x = i + 1;
+            while (true) {
+                if (x == nums.length) {
+                    x = 0;
                 }
-
+                if (nums[i] < hashMap.get(x)) {
+                    res[i] = hashMap.get(x);
+                    break;
+                }
+                if (x == i) {
+                    res[i] = -1;
+                    break;
+                }
+                x++;
             }
-
         }
-
         return res;
     }
+
+    // 太慢了
+    // public int[] nextGreaterElements(int[] nums) {
+    // int[] res = new int[nums.length];
+    // Deque<Integer> deque = new ArrayDeque<>();
+    // for (int i = 0; i < nums.length; i++) {
+    // deque.add(i);
+    // }
+    // Deque<Integer> stack = new ArrayDeque<>();
+    // for (int i = 0; i < nums.length; i++) {
+    // stack.clear();
+    // stack.addAll(deque);
+    // boolean flag = false;
+    // while (!stack.isEmpty()) {
+    // if (i == stack.peek()) {
+    // if (flag) {
+    // res[i] = -1;
+    // break;
+    // }
+    // stack.addLast(stack.pop());
+    // flag = true;
+    // }
+    // if (flag && nums[stack.peek()] > nums[i]) {
+    // res[i] = nums[stack.peek()];
+    // break;
+    // }
+    // stack.addLast(stack.pop());
+    // }
+    // }
+    // return res;
+    // }
 
 }
